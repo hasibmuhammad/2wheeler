7adb5be15b2cec0ccbfa5f8d06b8f0ee18612cc3
hasibmuhammad <hasibmu99@gmail.com>
2023-09-11 01:57:17 +0600

    Header, Featured - Done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+};
